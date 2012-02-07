cc1dd9f1f392d77ac1f32489d4955d41847d836f
mmtavares <mmtavares88@gmail.com>
2012-02-07 10:32:20 -0200

    animal

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Animalbom::Application.config.secret_token = '5ab6ae3dc9d0201f8f423ef61188e8d091394e9aff30eadc94540a8bda6bf2217a0a9bfbf1391684f45a71a6185d9a1deac2dc2d8c575682288034968bf43f2b'
